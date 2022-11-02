e754ead3d31519f2020321d388f426f400fa5c20
sugarmouse <tanghao19950911@gmail.com>
2022-11-02 16:42:49 +0800

    添加 URL 模块处理url 查询参数

--- index.ts
+++ index.ts
@@ -1,18 +1,26 @@
 import * as fs from 'fs';
 import * as http from 'http';
-import * as path from 'path';
-import { REPL_MODE_SLOPPY } from 'repl';
+import * as p from 'path';
+import * as url from 'url';
 
 const server = http.createServer();
-const publicDir = path.resolve(__dirname, 'public');
+const publicDir = p.resolve(__dirname, 'public');
 
 server.on("request", (request, response) => {
 
-  const { method, url, headers } = request;
-  switch (url) {
+  const { method, url: path, headers } = request;
+  // const pathObj = url.parse(path);
+  console.log(path);
+  
+
+  const { pathname, search } = path ? url.parse(path) : { pathname: 'index.html', search: '' };
+
+
+
+  switch (pathname) {
     case '/index.html':
       response.setHeader('Content-Type', 'text/html; charset=utf-8');
-      fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
         if (error) throw error;
         response.end(data.toString());
       });
@@ -20,7 +28,7 @@ server.on("request", (request, response) => {
 
     case '/style.css':
       response.setHeader('Content-Type', 'text/css; charset=utf-8');
-      fs.readFile(path.resolve(publicDir, 'style.css'), (error, data) => {
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
         if (error) throw error;
         response.end(data.toString());
       });
@@ -28,11 +36,15 @@ server.on("request", (request, response) => {
 
     case '/main.js':
       response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
-      fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
         if (error) throw error;
         response.end(data.toString());
       });
       break;
+
+    default:
+      response.statusCode = 404;
+      response.end();
   }
 });
 
